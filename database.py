2d5a6d2d485955dc646f5ef8ab688caff759e8ce
Участник 1. Кира <kira.korovkina@bk.ru>
2026-09-16 23:34:13 +0300

    feat: конфигурация через переменные окружения

--- database.py
+++ database.py
@@ -1,11 +1,20 @@
-from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
 
-from sqlalchemy import text
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./music_store.db")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def check_db_health():
-    """Проверяет соединение с БД. Возвращает True, если всё ок, иначе False."""
     try:
         db = SessionLocal()
         db.execute(text("SELECT 1"))
@@ -14,15 +23,3 @@ def check_db_health():
         return False
     finally:
         db.close()
-
-# ВАЖНО: Укажи правильное имя файла твоей базы данных!
-# Если в DBeaver подключение называется "bd ver1", то сам файл, скорее всего, называется "bd ver1.db" или "bd_ver1.db".
-# Проверь в проводнике Windows, как точно называется файл, и вставь его имя сюда.
-# Если файл лежит не в папке проекта, укажи полный путь (но лучше скопируй его в папку проекта).
-SQLALCHEMY_DATABASE_URL = "sqlite:///./music_store.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
